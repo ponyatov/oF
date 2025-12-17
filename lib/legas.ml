e15fd9c715c2bcde3a8893e30577ff047e07a317
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-17 12:23:00 +0400

    .

--- lib/legas.ml
+++ lib/legas.ml
@@ -1,8 +1,10 @@
 let app = Sys.getcwd () |> String.split_on_char '/' |> List.rev |> List.hd
 let title = "0F: zero FORTH in OCaml"
+
 let about = "
 - tiny REPL & compiler
 "
+
 let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
 let year = 2025
@@ -10,5 +12,11 @@ let version = "0.0.1"
 let license = "MIT"
 let github = "github: https://github.com/ponyatov/" ^ app
 
-#use "legas/files.ml"
-dirs();bins();giti();apt();readme()
+#use "legas/files.ml";
+files ();;
+
+#use "legas/vscode.ml";
+vscode ();
+
+#use "legas/git.ml";;
+git();;
