e6547f764c589e90d2c27daec7ffef1d247c3ab9
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-17 07:50:32 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# `of` 0.0.1
+## 0F: zero FORTH in OCaml
+
+(c) Dmitry Ponyatov <<dponyatov@gmail.com>> 2025 MIT
+
+github: https://github.com/ponyatov/of
+
+- tiny REPL & compiler
--- lib/legas.ml
+++ lib/legas.ml
@@ -1,6 +1,8 @@
 let app = Sys.getcwd () |> String.split_on_char '/' |> List.rev |> List.hd
 let title = "0F: zero FORTH in OCaml"
-let about = "tiny REPL & compiler"
+let about = "
+- tiny REPL & compiler
+"
 let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
 let year = 2025
@@ -9,4 +11,4 @@ let license = "MIT"
 let github = "github: https://github.com/ponyatov/" ^ app
 
 #use "legas/files.ml"
-dirs();
+dirs();bins();giti();apt();readme()
